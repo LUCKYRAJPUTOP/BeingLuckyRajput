a62bf84bc31998173624563ca433015f22cf047f
Anshul Rajput <82425167+LUCKYRAJPUTOP@users.noreply.github.com>
2021-05-05 08:18:41 +0530

    Update start.py

--- ULTRA/plugins/assistant/start.py
+++ ULTRA/plugins/assistant/start.py
@@ -63,8 +63,8 @@ async def start(event):
             caption=starttext,
             link_preview=False,
             buttons=[
-                [custom.Button.url("Dᴇᴘʟᴏʏ Yᴏᴜʀ Oᴡɴ UʟᴛʀᴀX", "http://GitHub.com/ULTRA-OP/ULTRA-X")],
-                [Button.url("Sᴜᴘᴘᴏʀᴛ", "t.me/UltraXchaT")],
+                [custom.Button.url("Dᴇᴘʟᴏʏ Yᴏᴜʀ Oᴡɴ UʟᴛʀᴀX", "http://GitHub.com/LuckyRajputOP/BeingLuckyRajput")],
+                [Button.url("Sᴜᴘᴘᴏʀᴛ", "t.me/BeingLuckyRajput")],
             ],
         )
         if os.path.exists(mypic):
